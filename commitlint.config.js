098e1308427ab0420232a0332741aa69be672ef4
gaoxiangqing <1356857447@qq.com>
2022-04-08 10:24:45 +0800

    build: commitlint-config scope add components

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,7 @@ module.exports = {
     extends: ['@commitlint/config-angular'],
     rules: {
         'header-max-length': [0, 'always', 120],
-        'scope-enum': [2, 'always', ['core', 'testing', 'config', 'auth', 'cache']],
+        'scope-enum': [2, 'always', ['core', 'testing', 'config', 'auth', 'cache', 'components']],
         'type-enum': [2, 'always', ['feat', 'fix', 'refactor', 'perf', 'docs', 'test', 'build', 'ci', 'revert']]
     }
 };
