757284bf35dad3f1be21c6cdeaa6467fa04b60ec
ggxxgxq <35676994+ggxxgxq@users.noreply.github.com>
2022-04-11 10:32:01 +0800

    build: add husky and commit hook and update commitlint version #INFR-… (#14)
    * build: add husky and commit hook and update commitlint version #INFR-2460
    
    * build: commitlint-config scope add components

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
+    extends: ['@commitlint/config-angular'],
     rules: {
-        'scope-enum': [2, 'always', ['core', 'testing', 'config', 'auth', 'cache']]
+        'header-max-length': [0, 'always', 120],
+        'scope-enum': [2, 'always', ['core', 'testing', 'config', 'auth', 'cache', 'components']],
+        'type-enum': [2, 'always', ['feat', 'fix', 'refactor', 'perf', 'docs', 'test', 'build', 'ci', 'revert']]
     }
 };
