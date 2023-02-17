81069f26aa8d07b8626a60b799cb826612fefa3e
zhangwen <1062680993@qq.com>
2023-02-17 09:59:09 +0800

    fix(components): fix dashboard widget component and delete itemRemovedCallback #INFR-6408 (#35)

--- packages/components/dashboard/widget/widget.component.ts
+++ packages/components/dashboard/widget/widget.component.ts
@@ -3,5 +3,12 @@ import { ThyWidgetItem } from '../dashboard.class';
 
 @Directive()
 export class ThyDashboardWidgetComponent<T = unknown> {
-    widget!: ThyWidgetItem<T>;
+    set widget(value: ThyWidgetItem<T>) {
+        this._widget = value;
+    }
+    get widget() {
+        return this._widget;
+    }
+
+    private _widget!: ThyWidgetItem<T>;
 }
