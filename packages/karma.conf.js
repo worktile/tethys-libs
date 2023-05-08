5e60275267906df425cfbd37e0153b2ca391b429
walkerkay <walkerkay@163.com>
2023-05-08 18:49:06 +0800

    build: change circleci config  #INFR-6739

--- packages/karma.conf.js
+++ packages/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
             suppressAll: true // removes the duplicated traces
         },
         coverageReporter: {
-            dir: '../coverage/@tethys',
+            dir: '../coverage/tethys',
             subdir: '.',
             reporters: [{ type: 'html' }, { type: 'text-summary' }]
         },
