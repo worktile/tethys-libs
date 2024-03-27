8f884ea602208fa20181a03f8afe0d1d2fc53037
why520crazy <why520crazy@163.com>
2024-03-27 10:07:19 +0800

    docs: remove import { ThyProLayoutModule } from '@tethys/pro/layout';

--- packages/components/countdown/examples/module.ts
+++ packages/components/countdown/examples/module.ts
@@ -1,6 +1,5 @@
 import { ThyProCountdownModule } from '@tethys/pro/countdown';
-import { ThyProLayoutModule } from '@tethys/pro/layout';
 
 export default {
-    imports: [ThyProCountdownModule, ThyProLayoutModule]
+    imports: [ThyProCountdownModule]
 };
