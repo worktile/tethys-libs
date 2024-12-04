50806837131d81a7e4cc61808a16dd299262b72d
luxiaobei <luxiaobei@worktile.com>
2024-12-04 15:46:20 +0800

    fix(components): fix position error when sortable in same entry #PJM-21290 (#156)
    * fix(components): fix position error when sortable in same entry #PJM-21290
    
    * fix(components): fix position error when sortable in same entry #PJM-21290

--- packages/components/board/entry/sortable/sortable.component.ts
+++ packages/components/board/entry/sortable/sortable.component.ts
@@ -66,9 +66,12 @@ export class ThyBoardSortableEntryComponent extends ThyBoardEntryBase {
     }
 
     drop(event: CdkDragDrop<ThyBoardDragContainer | SafeAny>) {
+        const droppedList = event.container.data?.cards.filter((card: ThyBoardCard) => {
+            return card._id !== event.item.data._id;
+        });
         const currentPreOrAfterCard = event.container.data.card;
         const currentPreOrAfterCardIndex =
-            (event.container.data.cards || []).findIndex((card: ThyBoardCard) => {
+            (droppedList || []).findIndex((card: ThyBoardCard) => {
                 return card._id === currentPreOrAfterCard?._id;
             }) || 0;
         const currentIndex = event.currentIndex ? currentPreOrAfterCardIndex + 1 : currentPreOrAfterCardIndex;
