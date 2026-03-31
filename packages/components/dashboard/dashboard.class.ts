ceb98585cb8650ddbeb92cb63b54e148551ccb62
luxiaobei <luxiaobei@worktile.com>
2026-03-31 16:10:08 +0800

    feat: upgrade @tethys/store@21.0.0-next.1 and angular-gridster2@21.0.0 (#209)

--- packages/components/dashboard/dashboard.class.ts
+++ packages/components/dashboard/dashboard.class.ts
@@ -1,5 +1,5 @@
 import { TemplateRef, Type } from '@angular/core';
-import { GridsterItem } from 'angular-gridster2';
+import { GridsterItemConfig } from 'angular-gridster2';
 import { SafeAny } from 'ngx-tethys/types';
 
 export type ThyWidgetViewOutlet<T = unknown> = Type<T> | TemplateRef<any>;
@@ -8,7 +8,7 @@ export type ThyWidgetViewOutletContext = Record<string, SafeAny>;
 
 export type ThyWidgetVieOutletWithContext<T = unknown> = { outlet: ThyWidgetViewOutlet<T>; context: ThyWidgetViewOutletContext };
 
-export interface WidgetGridsterItem extends GridsterItem {
+export interface WidgetGridsterItem extends GridsterItemConfig {
     widget: ThyWidgetItem;
     outlet: ThyWidgetViewOutlet;
     outletContext: ThyWidgetViewOutletContext;
